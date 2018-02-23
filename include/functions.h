669be061d7b50cac5b98a11c2263c0d48cb03589
Brendan Warnick <warnickb@eos07.cis.gvsu.edu>
2018-02-22 21:40:36 -0500

    finished iToP and added style guide

--- include/functions.h
+++ include/functions.h
@@ -3,6 +3,11 @@
 
 #include <iostream>
 #include <string.h>
+/**
+ * @author Brendan Warnick
+ * @version Winter 2018
+ * stacks and queues hw
+ */
 
 bool balancedSymbols(std::string input);
 std::string iToP(std::string input);
--- src/functions.cpp
+++ src/functions.cpp
@@ -2,13 +2,18 @@
 #include <iostream>
 #include <stack>
 /**
+ * @author Brendan Warnick
+ * @version Winter 2018
+ * stack and queue hw
+ */
+
+/**
  * perform check for matching items
  * used https://www.geeksforgeeks.org/check-for-balanced-parentheses-in-an-expression/ for refernce 
  */
 
 bool balancedSymbols(std::string input){
 	std::stack<char, std::deque<char> > testStack;
-	//go through stack
 	for(int i = 0; i < input.length(); i++){
 		//push starting characters
 		if(input[i] == '(' || input[i] == '[' || input[i] == '{'){
@@ -31,23 +36,40 @@ bool balancedSymbols(std::string input){
 	}
 	else return false;
 }
-
+/** 
+ * converts infix to postfix
+ */
 std::string iToP(std::string input){
+	//create stack and new string
 	std::stack<char, std::deque<char> > infixPostfix;
 	std::string newString;
 	for(int i = 0; i < input.length(); i++){
+		//add operators to start of queue
 		if(input[i] == '(' || input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/'){
-			infixPostfix.push(input[i];
+			infixPostfix.push(input[i]);
+		}
+		//add everything else to the queue
+		else newString += input[i];
+	}
+	//form new string with queue
+	while(!infixPostfix.empty()){
+		newString += infixPostfix.top();
+		infixPostfix.pop();
+	}
+	return newString;
+}
 
-
-
-
+/**
+ * main for testing a few samples
+ */
 
 int main(int argc, char* argv[]){
 	std::string testStuff = "({[test]}) [] () {()}";
 	std::string testStuff2 = "([){}}";
+	std::string testStuff3 = "a + b - c * d / e";
 	std::cout << balancedSymbols(testStuff) << std::endl;
 	std::cout << balancedSymbols(testStuff2) << std::endl;
+	std::cout << iToP(testStuff3) << std::endl;
 }
 
 
